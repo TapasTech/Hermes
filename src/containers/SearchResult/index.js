e22de9e784945bbfdf53f360cce1c77a86adb5e0
vagusX <vagusxl@gmail.com>
2016-03-02 15:54:42 +0800

    feat(container/search): done

--- src/containers/SearchResult/index.js
+++ src/containers/SearchResult/index.js
@@ -1,14 +1,66 @@
 import React from 'react';
 
-export default class SearchResult extends React.Component {
+import { TopicCard, NewestReports } from '#/components';
+
+import styles from './style.less';
+
+import { topicList } from '#/__mock__';
+
+export default class TopicList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentPage: 1
+    };
+  }
+
+  handleShowMore() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+  }
+
+  renderTopicCard(content, index) {
+    const { tag, topic } = content;
+    return (
+      <div className={styles.topic} key={index}>
+        <TopicCard content={content} />
+      </div>
+    );
+  }
+
+  renderQuestionList() {
+    const { q } = this.props.location.query;
+
+    const { currentPage } = this.state;
+    const list = [].concat(topicList);
+    const endIndex = currentPage * 10;
+    let onePage = list.slice(0, endIndex);
+
+    return (
+      <div className={styles.main}>
+        <div className={styles.title}>关于"{q}"的问题，共 {topicList.length} 条</div>
+        {
+          onePage.map((item, index) => this.renderTopicCard(item, index))
+        }
+        {
+          ((topicList.length > 10) && (endIndex < topicList.length))
+          ? <div className="more" onClick={::this.handleShowMore}>点击加载更多</div>
+          : <div className="end">已到结尾</div>
+        }
+      </div>
+    );
   }
 
   render() {
     return (
-      <div>
-        SearchResult Page
+      <div className="container">
+        <div className="main">
+          { this.renderQuestionList() }
+        </div>
+        <div className="sidebar">
+          <NewestReports />
+        </div>
       </div>
     );
   }
