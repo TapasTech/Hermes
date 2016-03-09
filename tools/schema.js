2d3015d23ad0d6a71dc590d40323bf425c35333f
Gerald <gera2ld@163.com>
2016-03-09 11:08:48 +0800

    feat(Tools): add deploy command

--- tools/schema.js
+++ tools/schema.js
@@ -7,7 +7,7 @@ const ensureDir = require('ensure-dir');
 new Promise((resolve, reject) => http.get('http://hermes-devel.dtcj.com/graphql', res => {
   var data = '';
   res
-  .on('data', (chunk) => data += chunk)
+  .on('data', chunk => data += chunk)
   .on('end', () => resolve(data))
   .on('error', err => reject(err));
 })).then(raw => {
